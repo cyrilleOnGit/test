5e180a2b083ee64304fff0a76b3196fa8a3a8cc6
Cyrille <cyrille.git@gmail.com>
2015-06-12 10:21:49 +0200

    Dans DataIO.java : ajout de la variable membre File file  ajout du constructeur qui crée README.txt s'il n'existe pas Dans Main.java: Une simple instanciation de DataIO

--- DataIO.java
+++ DataIO.java
@@ -1,5 +1,47 @@
 package activiteGit;
 
-public class DataIO {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class DataIO {
+	private File file;
+	
+	public DataIO(){
+		this.file = new File("");
+		String path = "";
+		char reponse = 'N';
+		Scanner scanPath = new Scanner(System.in);
+		
+		//Détermination du path de README.TXT
+		while(reponse != 'O'){
+			path = file.getAbsolutePath();
+			System.out.println("Le chemin courrant est \"" + path 
+					+ ".\nComplétez le au besoin :");
+			path=path.concat(scanPath.nextLine());
+			System.out.println("Le path du fichier README.TXT sera : " +path
+								+"\nS'il n'existe pas il sera créé !\n"
+								+ "Veuillez confirmer en saisissant 'O' :");
+			reponse = scanPath.nextLine().charAt(0);
+		}
+		scanPath.close();
+		System.out.println("Fait");
+		
+		if(path.charAt(path.length()-1)!='/'){
+			path = path.concat("/");
+		}
+		
+		//Création de README.txt
+		this.file = new File(path + "README.txt");
+		if(!this.file.exists()){
+			try {
+				file.createNewFile();
+				System.out.println("Création de " + this.file.getPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else{
+			System.out.println(this.file.getPath() + " existe déjà");
+		}
+	}
 }
--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ package activiteGit;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		DataIO fichierReadMe = new DataIO();
 	}
 
 }
