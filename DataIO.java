e01187d4b0bbffe9e8b15aa1e1eaa13883abea2f
Cyrille <cyrille.git@gmail.com>
2015-06-12 11:17:02 +0200

    Dans DataIO.java : ajout des variables membres DataInputStream dis, DataOutputStream dos, String message. ajout de la méthode public String readMessage(), qui lit le fichier README.txt stock la chaîne dans this.message et renvoie cette même chaîne. ajout de la méthode public void writeMessage (String message) qui prend en paramètre le chaîne à ajouter et met à jour README.txt.

--- DataIO.java
+++ DataIO.java
@@ -1,13 +1,26 @@
 package activiteGit;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class DataIO {
 	private File file;
+	private DataInputStream dis;
+	private DataOutputStream dos;
+	private String message;
 	
 	public DataIO(){
+		this.dis = null;
+		this.dos = null;
+		this.message = "";
 		this.file = new File("");
 		String path = "";
 		char reponse = 'N';
@@ -44,4 +57,69 @@ public class DataIO {
 			System.out.println(this.file.getPath() + " existe déjà");
 		}
 	}
+	
+	//méthode qui lit le Fichier README.txt
+	public String readMessage(){
+		//Ouverture du flux en lecture
+		try {
+			this.dis = new DataInputStream (
+							new BufferedInputStream (
+									new FileInputStream (this.file)));
+			
+			//lecture de la chaîne de caractères;
+			try {
+				this.message = dis.readUTF();
+			} catch (IOException e) {
+				System.out.println("fichier vide !");
+			} 
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		//fermeture du flux
+		}finally{
+			
+			if(this.dis != null){
+				try {
+					this.dis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		return this.message;
+	}
+	
+	public void writeMessage (String message){
+		//Ouverture du flux en écriture
+		try {
+			
+			this.dos = new DataOutputStream (
+					new BufferedOutputStream (
+							new FileOutputStream (this.file)));
+			
+			this.message = this.message.concat(message);
+			//Écriture dans README.txt;
+			try {
+				this.dos.writeUTF(this.message);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		//fermeture du flux
+		}finally{
+			
+			if(this.dos != null){
+				try {
+					this.dos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+	
 }
--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		DataIO fichierReadMe = new DataIO();
+		String data = "Ce fichier est rempli avec un DataOutputStream et lu avec un DataInputStream";
+		
+		fichierReadMe.writeMessage(data);
+		System.out.println(fichierReadMe.readMessage());
 	}
 
 }
